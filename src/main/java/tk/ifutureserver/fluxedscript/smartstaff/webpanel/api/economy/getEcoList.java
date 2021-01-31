bb568a4476aa6f86d2f057573f8adb8b9074ad76
FluxedScript <fluxedscript@gmail.com>
2021-01-31 09:45:55 +0000

    Clean up of getEcoList

--- src/main/java/tk/ifutureserver/fluxedscript/smartstaff/webpanel/api/economy/getEcoList.java
+++ src/main/java/tk/ifutureserver/fluxedscript/smartstaff/webpanel/api/economy/getEcoList.java
@@ -1,5 +1,8 @@
 package tk.ifutureserver.fluxedscript.smartstaff.webpanel.api.economy;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import net.milkbowl.vault.economy.Economy;
@@ -11,15 +14,20 @@ import tk.ifutureserver.fluxedscript.smartstaff.Main;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.*;
+import java.util.stream.Stream;
 
 public class getEcoList implements HttpHandler {
     Economy eco = Main.getEconomy();
+
     @SuppressWarnings("unchecked")
     public void handle(HttpExchange t) throws IOException {
         String oldpassword = Main.oldapipassword;
-        String sort_mode = null;
         String checkpassword = null;
         String response = t.getRequestURI().getQuery(); //if http://localhost:4000/api?name=john&password=hi
+
+        String responseBody = null;
+
         if(t.getRequestURI().getQuery() != null) {
             String[] parts = response.split("&");
             for (String s: parts) {
@@ -27,9 +35,6 @@ public class getEcoList implements HttpHandler {
                 String attribute = s.split("=")[1];
                 if (mainname.equalsIgnoreCase("password")) {
                     checkpassword = attribute;
-                }
-                if(mainname.equalsIgnoreCase("order") || mainname.equalsIgnoreCase("ordertype")) {
-                    sort_mode = attribute;
                 }
             }
             if (!(oldpassword.equals(checkpassword))) {
@@ -40,44 +45,26 @@ public class getEcoList implements HttpHandler {
                 os.close();
                 return;
             }
-            if(sort_mode != null) {
-                JSONObject main_json_obj = new JSONObject();
-                for (OfflinePlayer player : Bukkit.getOfflinePlayers()){
-                    JSONObject json_obj = new JSONObject();
-                    long balance = (long) eco.getBalance(player);
-                    json_obj.put("success", true);
-                    json_obj.put("uuid", player.getUniqueId());
-                    json_obj.put("last_played", player.getLastPlayed());
-                    json_obj.put("balance", balance);
-                    main_json_obj.put(player.getUniqueId(), json_obj.toString());
-                }
-
-                // do something with the request parameters
-                final String responseBody = main_json_obj.toString();
-                t.getResponseHeaders().set("Content-Type", "application/json");
-                final byte[] rawResponseBody = responseBody.getBytes(StandardCharsets.UTF_8);;
-                t.sendResponseHeaders(200, rawResponseBody.length);
-                t.getResponseBody().write(rawResponseBody);
-            } else{
-                JSONObject main_json_obj = new JSONObject();
-                for (OfflinePlayer player : Bukkit.getOfflinePlayers()){
-                    JSONObject json_obj = new JSONObject();
-                    long balance = (long) eco.getBalance(player);
-                    json_obj.put("success", true);
-                    json_obj.put("name", player.getName());
-                    json_obj.put("uuid", player.getUniqueId());
-                    json_obj.put("last_played", player.getLastPlayed());
-                    json_obj.put("balance", balance);
-                    main_json_obj.put(player.getUniqueId(), json_obj);
-                }
-
-                // do something with the request parameters
-                final String responseBody = main_json_obj.toString();
-                t.getResponseHeaders().set("Content-Type", "application/json");
-                final byte[] rawResponseBody = responseBody.getBytes(StandardCharsets.UTF_8);;
-                t.sendResponseHeaders(200, rawResponseBody.length);
-                t.getResponseBody().write(rawResponseBody);
+            JSONObject main_json_obj = new JSONObject();
+            for (OfflinePlayer player : Bukkit.getOfflinePlayers()){
+                JSONObject json_obj = new JSONObject();
+                long balance = (long) eco.getBalance(player);
+                json_obj.put("success", true);
+                json_obj.put("name", player.getName());
+                json_obj.put("uuid", player.getUniqueId());
+                json_obj.put("last_played", player.getLastPlayed());
+                json_obj.put("balance", balance);
+                main_json_obj.put(player.getUniqueId(), json_obj);
             }
+            responseBody = main_json_obj.toString();
+            // do something with the request parameters
+
+            t.getResponseHeaders().set("Content-Type", "application/json");
+            final byte[] rawResponseBody = responseBody.getBytes(StandardCharsets.UTF_8);
+            t.sendResponseHeaders(200, rawResponseBody.length);
+            t.getResponseBody().write(rawResponseBody);
         }
+
     }
+
 }
